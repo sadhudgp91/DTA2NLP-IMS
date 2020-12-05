acd45ee8d91d6062289671c689421c895a0304eb
kaushik sadhu <st158332@stud.uni-stuttgart.de>
2020-12-05 14:36:13 +0100

    added utilities folder for file conversion

---
+++ utilities/README.md
@@ -0,0 +1,11 @@
+# Utilities to convert TSV file to JSON and TEXT output to JSON
+
+1. for generation of json format from TSV input file
+2. for generation of json format from Text (Utf-8) input file
+
+Usage:
+python tsv2Json.py <tsv input filename>
+python txt2Json.py <txt input filename>
+
+Output 1: output_TSV2JSON.json
+Output 1: Output_tokens.json
---
+++ utilities/tsv2json.py
@@ -0,0 +1,35 @@
+# Author: Kaushik Sadhu
+# Script for TSV to JSON format conversion
+# IMS - University of Stuttgart 15.08.2020
+# Updated: September 06/2020
+# filename = enter the tsv filename in cmd when you start conversion command by calling python tsv2Json.py "filename"
+
+
+# Import lib
+
+import csv
+import json
+import collections
+import sys
+
+
+# aliases
+OrderedDict = collections.OrderedDict
+
+src = '/tmp/data.log'
+dst = '/tmp/data.json'
+
+# data input output
+
+data=[]
+filename = sys.argv[1]
+with open(filename,"r") as f:
+    firstline = f.readline()
+    columns = firstline.split()
+    lines = f.readlines()[1:]
+for line in lines:
+    values = line.split()
+    entry = dict(zip(columns, values))
+    data.append(entry)
+with open('output_TSV2JSON.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4, sort_keys = False) 
